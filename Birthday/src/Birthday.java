b146a6ed1c10fbd9ced6c58693c1351dd05bca31
edrenroan <boygreencross@gmail.com>
2019-09-03 09:05:49 +0800

    commit by edrenroan

---
+++ Birthday/src/Birthday.java
@@ -0,0 +1,40 @@
+/**
+ * @(#)Birthday.java
+ *
+ * Birthday application
+ *
+ * @author 
+ * @version 1.00 2019/9/3
+ */
+ import java.util.Scanner;
+import java.util.Date;
+import java.util.Calendar;
+import java.text.SimpleDateFormat;
+import java.util.GregorianCalendar;
+public class Birthday {
+    
+    public static void main(String[] args) throws Exception {
+    
+    Date date= new Date();
+    SimpleDateFormat format= new SimpleDateFormat("dd-M-yyyy");
+    Calendar calendar= new GregorianCalendar();
+    
+    String input_bday="21-09-1999";
+    Date bday= format.parse(input_bday);
+    
+    int year= calendar.get(Calendar.YEAR);
+    calendar.setTime(bday);
+    int byear= calendar.get(Calendar.YEAR);
+    
+    int age= year-byear;
+    System.out.println("My age: " + age);
+    
+    
+    
+    
+    
+    
+    
+    	
+    }
+}
